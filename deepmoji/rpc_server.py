d911cd91f993b26b3a2dbb5f3efa91206edc3610
ionorins <ion.orins@modex.tech>
2021-02-16 22:28:20 +0200

    added script to run deepmoji locally

--- deepmoji/rpc_server.py
+++ deepmoji/rpc_server.py
@@ -1,28 +1,29 @@
+import json
+from time import sleep
+
+import pika
+
+from deepmoji.global_variables import PRETRAINED_PATH, VOCAB_PATH
+from deepmoji.model_def import deepmoji_emojis
+from deepmoji.sentence_tokenizer import SentenceTokenizer
+
+# set up deep moji
+maxlen = 30
+batch_size = 32
+
+with open(VOCAB_PATH, "r") as f:
+    vocabulary = json.load(f)
+st = SentenceTokenizer(vocabulary, maxlen)
+
+model = deepmoji_emojis(maxlen, PRETRAINED_PATH)
+model.summary()
+
+
 while True:
     try:
-        import json
-        from time import sleep
-
-        import pika
-
-        from deepmoji.global_variables import PRETRAINED_PATH, VOCAB_PATH
-        from deepmoji.model_def import deepmoji_emojis
-        from deepmoji.sentence_tokenizer import SentenceTokenizer
-
-        # set up deep moji
-        maxlen = 30
-        batch_size = 32
-
-        with open(VOCAB_PATH, "r") as f:
-            vocabulary = json.load(f)
-        st = SentenceTokenizer(vocabulary, maxlen)
-
-        model = deepmoji_emojis(maxlen, PRETRAINED_PATH)
-        model.summary()
-
         # set up rmq
         connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host="rabbitmq"))
+            pika.ConnectionParameters(host="localhost"))
 
         channel = connection.channel()
 
@@ -39,15 +40,16 @@ while True:
             response = analyse(body)
 
             ch.basic_publish(exchange="",
-                             routing_key=props.reply_to,
-                             properties=pika.BasicProperties(
-                                 correlation_id=props.correlation_id),
-                             body=str(response))
+                                routing_key=props.reply_to,
+                                properties=pika.BasicProperties(
+                                    correlation_id=props.correlation_id),
+                                body=str(response))
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
         channel.basic_qos(prefetch_count=1)
         channel.basic_consume(queue="deepmoji", on_message_callback=on_request)
 
         channel.start_consuming()
-    except:
-        sleep(0.5)
+    except Exception as e:
+        print(e)
+        sleep(3)
