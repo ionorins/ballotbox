a1176186edee53d0f721c959145413632e75424e
ionorins <ion.orins@modex.tech>
2021-02-23 20:38:38 +0200

    fixed ensembleLearner bug

--- emotion_analysis/ensembleLearner.py
+++ emotion_analysis/ensembleLearner.py
@@ -24,14 +24,17 @@ class ensembleLearner:
         elif text == "" and np.count_nonzero(distEmoji) != 0:
             emojiVector = distEmoji
 
+        else:
+            emojiVector = distEmoji
+
         complexEmotion = self.j2c.dot(emojiVector)
         return(complexEmotion)
 
 
 # # Some Extravagant Examples!
 
-# el = ensembleLearner()
-# distEmoji = np.zeros(64)
-# # distEmoji[14] = 1
-# print(el.getComplexEmotion(distEmoji, "dafuq is dis shyeet"))
+el = ensembleLearner()
+distEmoji = np.zeros(64)
+# distEmoji[14] = 1
+print(el.getComplexEmotion(distEmoji, ""))
 # print(el.getComplexEmotion(distEmoji, "this is just spectacular"))    
